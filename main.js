cd3e16dc6b2b6a53a5b73914b99a5841a422cace
Ev1son <evison.pacheco21@gmail.com>
2022-04-16 15:42:40 -0300

    feature: botão ver mais adicionado

---
+++ main.js
@@ -0,0 +1,19 @@
+const button = document.querySelector(".abstract-expandable__button-js");
+
+button.onclick = () => { seeMore() };
+
+function seeMore() {
+  let points = document.querySelector(".abstract-expandable__points-js");
+  let more = document.querySelector(".abstract-expandable-more-js");
+  let btnSeeMore = document.querySelector(".abstract-expandable__button-js");
+
+  if (points.style.display === "none") {
+    points.style.display = "inline";
+    more.style.display = "none";
+    btnSeeMore.innerHTML = "ver mais";
+  } else {
+    points.style.display = "none";
+    more.style.display = "block";
+    btnSeeMore.innerHTML = "";
+  }
+}
