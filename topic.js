2ced6aabc7af04fe0cd88a1f05b6f5de4f04a9fa
Ev1son <evison.pacheco21@gmail.com>
2022-04-18 01:22:44 -0300

    update topic.js: for mudado de lugar

--- topic.js
+++ topic.js
@@ -1,16 +1,16 @@
 const topicList = document.querySelectorAll(".topic-clickable-js");
 const expandedList = document.querySelectorAll(".expanded-js");
 
+for (let index = 0; index < topicList.length; index++) {
+  const topicIndex = topicList[index];
+  
+  topicIndex.addEventListener("click", () => { showContent(index) });
+}
+
 function showContent(index) {
   if (expandedList[index].style.display === "block") {
     expandedList[index].style.display = "none" 
   } else {
     expandedList[index].style.display = "block"
   }
-}
-
-for (let index = 0; index < topicList.length; index++) {
-  const topicIndex = topicList[index];
-  
-  topicIndex.addEventListener("click", () => { showContent(index) });
 }
