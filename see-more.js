67ace1a9d624b62628b2994693528f585a04b42d
Ev1son <evison.pacheco21@gmail.com>
2022-04-18 01:23:06 -0300

    update see-more.js: retirado condição

--- see-more.js
+++ see-more.js
@@ -7,13 +7,7 @@ function seeMore() {
   let more = document.querySelector(".abstract-expandable-more-js");
   let btnSeeMore = document.querySelector(".abstract-expandable__button-js");
 
-  if (points.style.display === "none") {
-    points.style.display = "inline";
-    more.style.display = "none";
-    btnSeeMore.innerHTML = "ver mais";
-  } else {
-    points.style.display = "none";
-    more.style.display = "block";
-    btnSeeMore.innerHTML = "";
-  }
+  points.style.display = "none";
+  more.style.display = "block";
+  btnSeeMore.innerHTML = "";
 }
